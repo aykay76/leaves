08b1237305c83370a35317e34daaf50b83f23340
Alan Kelly <alan.kelly.london@gmail.com>
2022-01-17 13:20:45 +0000

    adding scrollIntoView fixed positioning with flex

--- internal/static/leaves.js
+++ internal/static/leaves.js
@@ -1,28 +1,14 @@
-// import { SVGDraw } from './svgdraw.js'
-
-// var svgdraw = new SVGDraw(document.getElementById('board'))
-// svgdraw.loadModel()
-
-// function doClear() {
-//     svgdraw.resetModel()
-// }
-
-// function undo() {
-//     svgdraw.undo()
-// }
-
-// function redo() {
-//     svgdraw.redo()
-// }
-
 function selectMenu() {
     let content = window.event.target.getAttribute('content')
+    let replace = window.event.target.getAttribute('replace')
 
-    // could remove everything, or add to the home page
-    // let container = document.getElementById('mainleaf')
-    // while (container.firstChild) {
-    //     container.removeChild(container.lastChild)
-    // }
+    // if there is an attribute set to replace the content we will remove all leaves
+    if (replace) {
+        let container = document.getElementById('leaf-container')
+        while (container.firstChild) {
+            container.removeChild(container.lastChild)
+        }
+    }
 
     fetch(`/${content}`)
     .then(response => {
@@ -31,23 +17,20 @@ function selectMenu() {
     .then(content => {
         let newdiv = document.createElement('div')
         newdiv.classList.add('leaf')
-        newdiv.style.width = (window.innerWidth - 150) + 'px'
-        newdiv.style.height = '100%'
+        newdiv.style.width = window.innerWidth + 'px'
         newdiv.innerHTML = content
-        document.getElementById('mainleaf').appendChild(newdiv)
+        document.getElementById('leaf-container').appendChild(newdiv)
 
         Array.from(newdiv.getElementsByTagName('a')).forEach(el => {
             el.addEventListener('click', () => { clickLeaf() })
         })
+
+        newdiv.scrollIntoView({behavior: "smooth", block: "center", inline: "nearest"})
     })
 }
 
 function clickLeaf() {
     window.event.cancelBubble = true
-
-    console.dir(document.getElementById('mainleaf'))
-    let width = document.getElementById('mainleaf').clientWidth
-    console.log(width)
 
     let href = window.event.target.getAttribute('content')
     fetch(`/${href}`)
@@ -57,16 +40,13 @@ function clickLeaf() {
     .then(content => {
         let newdiv = document.createElement('div')
         newdiv.classList.add('leaf')
-        newdiv.style.width = (window.innerWidth - 100) + 'px'
-        newdiv.style.height = '100%'
+        newdiv.style.width = window.innerWidth + 'px'
         newdiv.innerHTML = content
-        document.getElementById('mainleaf').appendChild(newdiv)
+        document.getElementById('leaf-container').appendChild(newdiv)
+
+        newdiv.scrollIntoView({behavior: "smooth", block: "center", inline: "nearest"})
     })
 }
-
-// document.getElementById('clear').addEventListener('click', () => { doClear() })
-// document.getElementById('undo').addEventListener('click', () => { undo() })
-// document.getElementById('redo').addEventListener('click', () => { redo() })
 
 Array.from(document.getElementsByClassName('menuitem')).forEach(el => {
     el.addEventListener('click', () => { selectMenu() })
---
+++ readme.md
@@ -0,0 +1,3 @@
+
+After following a few articles on how to do horizontal scrolling, the simplest and best working answer I found here:
+https://medium.com/@tommybernaciak/css-horizontal-scroll-25625e65af9
