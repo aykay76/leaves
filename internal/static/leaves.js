3f77621e0e7dad6704a63d00dd98d8e3eadc13c1
Alan Kelly <alan.kelly.london@gmail.com>
2022-01-19 21:59:18 +0000

    add to crumb trail

--- internal/static/leaves.js
+++ internal/static/leaves.js
@@ -1,4 +1,5 @@
 function addToBreadcrumb() {
+    // TODO: make crumb text an attribute
     let content = window.event.target.getAttribute('content')
     let replace = window.event.target.getAttribute('replace')
 
@@ -14,12 +15,12 @@ function addToBreadcrumb() {
     .then(response => {
         return response.text()
     })
-    .then(content => {
+    .then(html => {
         let newdiv = document.createElement('div')
         newdiv.classList.add('leaf')
         newdiv.style.width = window.innerWidth + 'px'
         newdiv.style.height = (window.innerHeight - 30) + 'px'
-        newdiv.innerHTML = content
+        newdiv.innerHTML = html
         document.getElementById('leaf-container').appendChild(newdiv)
 
         Array.from(newdiv.getElementsByClassName('menuitem')).forEach(el => {
@@ -27,6 +28,12 @@ function addToBreadcrumb() {
         })
         
         newdiv.scrollIntoView({behavior: "smooth", block: 'end' })
+
+        // TODO: check if not first crumb, add separator
+        let crumb = document.createElement('span')
+        crumb.classList.add('crumb')
+        crumb.innerText = content
+        document.getElementById('crumb-trail').appendChild(crumb)
     })
 }
 
