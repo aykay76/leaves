217e998eaf2995ad14b02eda9bf1561584122ac4
Alan Kelly <alan.kelly.london@gmail.com>
2022-02-07 19:09:11 +0000

    create stack of crumbs and leaves add links to remove leaves

--- internal/static/leaves.js
+++ internal/static/leaves.js
@@ -1,3 +1,6 @@
+let leafstack = []
+let crumbstack = []
+
 function addCrumbToTrail() {
     // TODO: make crumb text an attribute
     let content = window.event.target.getAttribute('content')
@@ -23,8 +26,13 @@ function addCrumbToTrail() {
         newdiv.innerHTML = html
         document.getElementById('leaf-container').appendChild(newdiv)
 
+        leafstack.push(newdiv)
+        console.log(leafstack)
+
+        // TODO: don't just blindly add, remove everything after this page and add
+        // same as if you go back in your browser history and then click on a link
         Array.from(newdiv.getElementsByClassName('menuitem')).forEach(el => {
-            el.addEventListener('click', () => { addCrumbToTrail() })
+            el.addEventListener('click', () => { addCrumbToTrail(newdiv) })
         })
         
         newdiv.scrollIntoView({behavior: "smooth", block: 'end' })
@@ -37,16 +45,33 @@ function addCrumbToTrail() {
         // TODO: keep a stack of crumbs
         // make the top crumb a link to a function that will remove everything to that point
         // then as the trail builds each crumb will remove an increasing number of leaves to revert to that point
-
-        let crumb = document.createElement('span')
-        crumb.classList.add('crumb')
-        crumb.innerText = content
-        document.getElementById('crumb-trail').appendChild(crumb)
+        addCrumb(content)
     })
 }
 
-function removeCrumbsFromTrail() {
+function addCrumb(content) {
+    if (crumbstack.length > 0) {
+        let prevcrumb = crumbstack[crumbstack.length - 1]
 
+        let link = document.createElement('a')
+        link.href = "javascript:void(0);"
+        link.innerText = prevcrumb.innerText
+        link.addEventListener('click', () => { removeCrumbsFromTrail() })
+        prevcrumb.innerText = ""
+        prevcrumb.appendChild(link)
+        console.log(`Making previous crumb a link to prune the trail ${prevcrumb.innerText}`)
+    }
+
+    let crumb = document.createElement('span')
+    crumb.classList.add('crumb')
+    crumb.innerText = content
+    document.getElementById('crumb-trail').appendChild(crumb)
+    crumbstack.push(crumb)
+    console.log(crumbstack)
+}
+
+function removeCrumbsFromTrail() {
+    console.log('removing crumb(s) from trail')
 }
 
 Array.from(document.getElementsByClassName('menuitem')).forEach(el => {
@@ -65,3 +90,4 @@ Array.from(document.getElementsByClassName('leaf')).forEach(el => {
 })
 
 document.getElementById('leaf-container').style.height = (window.innerHeight - 30) + 'px'
+addCrumb('Home')
